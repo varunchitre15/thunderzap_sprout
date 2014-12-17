d0eb16cc4ac9d18afa1cf5793b8a01cd0f691f62
Varun Chitre <varun.chitre15@gmail.com>
2014-12-17 18:53:15 +0100

    clean script update

--- clean.sh
+++ clean.sh
@@ -1,2 +1,8 @@
-make ARCH=arm CROSS_COMPILE=~/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi- -j8 clean mrproper
+KERNEL_DIR=$PWD
+ZIMAGE=$KERNEL_DIR/arch/arm/boot/zImage
+make ARCH=arm CROSS_COMPILE=/root/cm11/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi- -j8 clean mrproper
+if [ -a $ZIMAGE ];
+then
+rm $ZIMAGE $KERNEL_DIR/ramdisk.cpio $KERNEL_DIR/root.fs $KERNEL_DIR/boot.img
+fi
 rm include/linux/autoconf.h
