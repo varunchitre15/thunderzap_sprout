1ba17b538676b2a28d5c1c764c8c9299d7112c58
Varun Chitre <varun.chitre15@gmail.com>
2014-12-19 19:08:41 +0100

    don't remove zImage

--- clean.sh
+++ clean.sh
@@ -1,8 +1,5 @@
 KERNEL_DIR=$PWD
 ZIMAGE=$KERNEL_DIR/arch/arm/boot/zImage
 make ARCH=arm CROSS_COMPILE=/root/cm11/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi- -j8 clean mrproper
-if [ -a $ZIMAGE ];
-then
-rm $ZIMAGE $KERNEL_DIR/ramdisk.cpio $KERNEL_DIR/root.fs $KERNEL_DIR/boot.img
-fi
+rm $KERNEL_DIR/ramdisk.cpio $KERNEL_DIR/root.fs $KERNEL_DIR/boot.img
 rm include/linux/autoconf.h
