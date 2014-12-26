b9dcf82edc0952f5625cc5ba210332b848a6b117
Varun Chitre <varun.chitre15@gmail.com>
2014-12-26 17:42:50 +0100

    build: add support for colored output and branding

--- build.sh
+++ build.sh
@@ -21,6 +21,12 @@ MKBOOTFS=$KERNEL_DIR/tools/mkbootfs
 ROOTFS=$KERNEL_DIR/root.fs
 BOOTIMG=$KERNEL_DIR/boot.img
 BUILD_START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+# Modify the following variable if you want to build
 export CROSS_COMPILE="/root/cm11/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-"
 export ARCH=arm
 export SUBARCH=arm
@@ -29,31 +35,42 @@ export KBUILD_BUILD_HOST="Monster-Machine"
 
 compile_kernel ()
 {
+echo -e "$blue***********************************************"
+echo "          Compiling ThunderZap kernel          "
+echo -e "***********************************************$nocol"
 make sprout_defconfig
 make -j32
 if ! [ -a $ZIMAGE ];
 then
-echo "Kernel Compilation failed! Fix the errors!"
+echo -e "$redKernel Compilation failed! Fix the errors!$nocol"
 exit 1
 fi
 }
 
 compile_bootimg ()
 {
-echo "Creating boot image for $2"
+echo -e "$yellow*************************************************"
+echo "             Creating boot image for $2"
+echo -e "*************************************************$nocol"
 $MKBOOTFS $1/ > $KERNEL_DIR/ramdisk.cpio
 cat $KERNEL_DIR/ramdisk.cpio | gzip > $KERNEL_DIR/root.fs
 $MKBOOTIMG --kernel $ZIMAGE --ramdisk $KERNEL_DIR/root.fs  --base 0x80000000 --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --tags_offset 0x00000100 --output $KERNEL_DIR/boot.img
 if ! [ -a $ROOTFS ];
 then
-echo "Ramdisk creation failed"
+echo -e "$redRamdisk creation failed$nocol"
 exit 1
 fi
 if ! [ -a $BOOTIMG ];
 then
-echo "Boot image creation failed"
+echo -e "$redBoot image creation failed$nocol"
 exit 1
 fi
+finally_done
+}
+
+finally_done ()
+{
+echo -e "$cyan BOOT Image installed on: $BOOTIMG$nocol"
 }
 
 case $1 in
@@ -75,10 +92,10 @@ rm -rf $KERNEL_DIR/ramdisk.cpio $KERNEL_DIR/root.fs $KERNEL_DIR/boot.img
 rm -rf include/linux/autoconf.h
 ;;
 *)
-echo "Add valid option"
+echo -e "Add valid option\nValid options are:\n./build.sh (stock|cm11|cm12|clean)"
 exit 1
 ;;
 esac
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
-echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
